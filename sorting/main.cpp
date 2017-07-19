d8f743c7761a21be06e28291eaa46f45c1ff7658
PrincessKenny <bendez95@gmail.com>
2017-07-19 02:18:45 +0200

    submit

--- sorting/main.cpp
+++ sorting/main.cpp
@@ -3,6 +3,9 @@
 #include "task2.h"
 using namespace std;
 int main() {
+	Data cdata;
+	cdata.read_data("pop_density.txt");
+	MergeSort::merger(cdata);
 	Number_Data ndata("integers.txt");
 	RadixSort::rsort(ndata);
 	for (int i = 0; i < 100; i++)
